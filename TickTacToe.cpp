144a5476197346beb9c9b3f2dbeb1e9cfe6f66ad
Memoona Mobeen <memoonamobeen618@gmail.com>
2023-07-11 19:03:40 +0500

    Prob C

---
+++ TickTacToe.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    int total_ways = n * n + 7 * n;
+
+    std::cout << total_ways << std::endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+ 
