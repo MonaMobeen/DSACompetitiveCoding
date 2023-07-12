a5dafaa4eb9209ff40912ad33e20b1a1b3170729
Memoona Mobeen <memoonamobeen618@gmail.com>
2023-07-12 03:04:33 +0000

    adding Another Approach

--- ScaleAiDSA/Gym/TickTacToe.cpp
+++ ScaleAiDSA/Gym/TickTacToe.cpp
@@ -1,12 +1,47 @@
+// #include <iostream>
+
+// int main() {
+//     int n;
+//     std::cin >> n;
+
+//     int total_ways = n * n + 7 * n;
+
+//     std::cout << total_ways << std::endl;
+
+//     return 0;
+// }
+
 #include <iostream>
 
-int main() {
-    int n;
-    std::cin >> n;
+using namespace std;
 
-    int total_ways = n * n + 7 * n;
+int count_ways(int n) {
+ /*
+ Counts the number of ways to put tokens in a line in a 3D n × n × n cube.
 
-    std::cout << total_ways << std::endl;
+ Args:
+   n: The side length of the cube.
 
-    return 0;
+ Returns:
+   The number of ways to put tokens in a line.
+ */
+
+ int ways = 0;
+ for (int i = 0; i < n; i++) {
+   ways += 6;
+   ways += 12;
+   ways += 4;
+ }
+
+ return ways;
 }
+
+int main() {
+ int n;
+ cin >> n;
+
+ cout << count_ways(n) << endl;
+
+ return 0;
+}
+
